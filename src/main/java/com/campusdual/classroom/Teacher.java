2ac2202396bf65d81dad49674ee42a2877a82306
NotOppi <Filippofg@outlook.com>
2025-05-26 10:27:52 +0200

    Listo

--- src/main/java/com/campusdual/classroom/Teacher.java
+++ src/main/java/com/campusdual/classroom/Teacher.java
@@ -8,4 +8,10 @@ public class Teacher extends Person {
 		super(name, surname);
 		this.area = area;
 	}
+
+	@Override
+	public void getDetails() {
+		System.out.println("Doctor " + name + " " + surname + ", del area de " + area.toLowerCase());
+	}
+
 }
